fe8bac2eef12a6e307f8861cc600776c490f3b02
mamad <67847640+mamad1313fld@users.noreply.github.com>
2024-08-06 13:17:38 +0330

    Update ad.py

--- ad.py
+++ ad.py
@@ -1,5 +1,5 @@
-// Copyright (c) 2024 mamad1313fld
-// Licensed under the MIT License. See LICENSE file in the project root for full license information.
+# Copyright (c) 2024 mamad1313fld
+# Licensed under the MIT License. See LICENSE file in the project root for full license information.
 import random
 import time
 
